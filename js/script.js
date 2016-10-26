d24e534550780deb8865a0c99a0f3efef1ee28d3
Chris Wingo and Stephan Khoury <stephan@>
2016-10-26 10:20:48 -0700

    Pass the first spec. Is the letter a vowel?

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
 
-    
+    var userInput = $("input#english").val();
+
+    console.log(isVowel(userInput[0]));
   });
 });
+
+function isVowel(letter){
+  var vowels = ('aeiou');
+
+    if (vowels.indexOf(letter) > -1) {
+      return true;
+    } else {
+      return false;
+    }
+}
